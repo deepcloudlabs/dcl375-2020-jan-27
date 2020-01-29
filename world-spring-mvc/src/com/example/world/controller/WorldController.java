9d499e3c82465bab48a2a020d6edc3aa7b48eddb
deepcloudlabs <34dmng65jm>
2020-01-29 14:52:49 +0300

    Use @ModelAttribute to put continents into Model so that home.jsp can render select box containing list of continents

--- world-spring-mvc/src/com/example/world/controller/WorldController.java
+++ world-spring-mvc/src/com/example/world/controller/WorldController.java
@@ -1,17 +1,16 @@
 package com.example.world.controller;
 
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.world.dao.CountryDao;
-import com.example.world.entity.Country;
 
 /**
  * 
@@ -24,20 +23,20 @@ public class WorldController {
 	@Autowired
 	private CountryDao countryDao;
 
+	@ModelAttribute("continents")
+	public Set<String> getAllContinents() {
+		return countryDao.getAllContinents();
+	}
+
 	@GetMapping
-	public String home(Model model) {
-		Set<String> continents = countryDao.getAllContinents();
-		model.addAttribute("continents", continents);
+	public String home() {
 		return "home";
 	}
+
 	@PostMapping
-	public String list(Model model,String continent) {
-		List<Country> countries = 
-				countryDao.findCountriesByContinent(
-						continent);
-		model.addAttribute("countries", countries);
-		Set<String> continents = countryDao.getAllContinents();
-		model.addAttribute("continents", continents);
-		return "home";
+	public ModelAndView list(ModelAndView mv, String continent) {
+		mv.setViewName("home");
+		mv.getModelMap().addAttribute("countries", countryDao.findCountriesByContinent(continent));
+		return mv;
 	}
 }
